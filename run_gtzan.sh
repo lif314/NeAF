2e3c983c7ecb50693dbd627773ba79c4924f2194
lif314 <cslinfeili@gmail.com>
2024-06-28 09:55:44 +0800

    update gtzan eval scripts

--- run_gtzan.sh
+++ run_gtzan.sh
@@ -8,14 +8,17 @@ base_path=data/gtzan/genres
 data_path=$base_path/$type/$type.$seq_id.wav
 save_dir=logs/gtzan/$type/$seq_id
 
-# 263 K
-python train.py --arch siren \
-    --save_dir $save_dir \
-    --exp_name siren \
-    --audio_path $data_path
-
 # 254 K
 python train.py --arch fourier \
     --save_dir $save_dir \
     --exp_name fourier \
+    --batch_size 16384 \
+    --audio_path $data_path
+
+
+# 263 K
+python train.py --arch siren \
+    --save_dir $save_dir \
+    --exp_name siren \
+    --batch_size 16384 \
     --audio_path $data_path
--- run_gtzan_batch.sh
+++ run_gtzan_batch.sh
@@ -4,13 +4,15 @@
 # seq_id=00000
 
 
-types=(blues classical country disco hiphop jazz metal pop reggae rock)
-seq_ids=(00000  00033 00099)
+# types=(blues classical country disco hiphop jazz metal pop reggae rock)
+types=(classical country disco hiphop jazz metal pop reggae rock)
+seq_ids=(00000 00033 00099)
 
-for type in $types
+
+for type in "${types[@]}"
 do
-    for seq_id in $seq_ids
+    for seq_id in "${seq_ids[@]}"
     do
-        bash run_gtzan.sh $type $seq_id
+        bash run_gtzan.sh "$type" "$seq_id"
     done
 done
